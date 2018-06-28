11fd857318e595a150c34cac27ba5d3d362b742e
fabritsius <antonfabritsius@gmail.com>
2018-06-28 13:55:14 +0300

    fix: correct the activation procedure of service worker
    The previous version of activation event listener was whitelisting everything and that means 0 (zero) cleaning of old caches. I also removed caching when fetching requests (preparation before IndexedDB implementation).

--- sw.js
+++ sw.js
@@ -10,7 +10,8 @@ const urlsToCache = [
   'js/main.js',
   'js/restaurant_info.js',
   'js/dbhelper.js',
-  'js/swhelper.js'
+  'js/swhelper.js',
+  'icons/favicon.ico'
 ];
 
 // Perform install steps
@@ -30,36 +31,50 @@ self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
       // Fetch the response if it is not cached 
-      return response || fetch(event.request).then(networkResponse => {
-        // Cache the response only if it is useful
-        if (networkResponse && networkResponse.status === 200) {
-          // Clone the response cause it is a stream
-          const clonedResponse = networkResponse.clone();
-
-          caches.open(CACHE_NAME)
-            .then((cache) => {
-              cache.put(event.request.url, clonedResponse);
-            })
-        }
-        // Return the response in any scenario
-        return networkResponse;
-      });
+      return response || fetch(event.request)
     })
   );
 });
+// self.addEventListener('fetch', (event) => {
+//   // console.log(`fetch request: ${event.request.url}`)
+//   event.respondWith(
+//     caches.match(event.request).then((response) => {
+//       // Fetch the response if it is not cached 
+//       return response || fetch(event.request).then(networkResponse => {
+//         // Cache the response only if it is useful
+//         if (networkResponse && networkResponse.status === 200) {
+//           // Clone the response cause it is a stream
+//           const clonedResponse = networkResponse.clone();
+//           caches.open(CACHE_NAME)
+//             .then((cache) => {
+//               cache.put(event.request.url, clonedResponse);
+//             })
+//         }
+//         // Return the response in any scenario
+//         return networkResponse;
+//       });
+//     })
+//   );
+// });
 
 // Delete outdated caches
 self.addEventListener('activate', (event) => {
-  var cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
+        cacheNames.filter((cacheName) => {
+          return cacheName.startsWith(APP_NAME) &&
+                 CACHE_NAME != cacheName;
+        }).map((cacheName) => {
+          return caches.delete(cacheName);
         })
       );
     })
   );
+});
+
+self.addEventListener('message', function(event) {
+  if (event.data.action === 'skipWaiting') {
+    self.skipWaiting();
+  }
 });
